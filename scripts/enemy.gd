4dbdb2f8157167dc5885603d569b05ccc191b425
Zaydao <mr.eelviaan@mail.ru>
2024-07-22 22:25:03 +0300

    modificated enemy navigation and tiles

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,18 +1,15 @@
 extends CharacterBody2D
 
-const speed = 200
+var speed = 200
+
+@export var player: Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
 
-var hp
-var player: Node2D
+var hp 
+
 
 func _ready():
 	hp = Global.enemy1_health
-	player = get_node("/root/world/player") as Node2D
-	if player:
-		makepath()
-	else:
-		print("Player node is not found at path: /root/world/player")
 
 func take_damage():
 	hp -= Global.bullet1_dm
@@ -21,22 +18,13 @@ func take_damage():
 		queue_free()
 
 func _physics_process(_delta: float) -> void:
-	if player:
-		# Rotește dușmanul să se uite spre jucător
-		look_at(player.global_position)
-
-		# Calculează direcția către jucător și mișcă dușmanul în acea direcție
-		var direction = (player.global_position - global_position).normalized()
-		velocity = direction * speed
-		move_and_slide()
-	else:
-		print("Player node is not assigned.")
-
+	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = dir * speed
+	move_and_slide()
+	
+	
 func makepath() -> void:
-	if player:
-		nav_agent.target_position = player.global_position
-	else:
-		print("Player node is not assigned.")
+	nav_agent.target_position = player.global_position
 
 func _on_timer_timeout():
 	makepath()
